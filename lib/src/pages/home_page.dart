37aa8c342e7ab843e34a0749806d3b7b00e11f4e
awss197812 <wsalcedo78@gmail.com>
2019-10-07 23:14:22 -0500

    Mostrar el póster de películas

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -4,7 +4,8 @@ import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+  
+  final peliculasProvider = new PeliculasProvider();
 
   @override
   Widget build(BuildContext context) {
@@ -34,13 +35,31 @@ class HomePage extends StatelessWidget {
 
   Widget _swiperTarjetas() {
 
-    final peliculasProvider = new PeliculasProvider();
+    return FutureBuilder(
+      future: peliculasProvider.getEnCines(),
+      //initialData: InitialData,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if(snapshot.hasData){
+          return CardSwiper(
+            peliculas: snapshot.data,
+          );
+        }else{
+          return Container(
+            height: 400.0,
+            child: Center(
+              child: CircularProgressIndicator()
+              ),
+          );
+        }
+      },
+    );
 
-    peliculasProvider.getEnCines();
 
+    /*
     return CardSwiper(
 
       peliculas: [1,2,3,4,5],
     );
+    */
   }
 }
