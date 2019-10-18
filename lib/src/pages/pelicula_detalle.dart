7845db6e301ae33305466c82732d377c5241ac91
mios260410 <mios260410@iMac-de-desarrollo.local>
2019-10-18 16:59:49 -0500

    Pantalla de detalle de películas

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -9,9 +9,20 @@ class PeliculaDetalle extends StatelessWidget {
     final Pelicula pelicula = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-      body: Center(
-        child: Text(pelicula.title),
-      ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          _crearAppbar(pelicula),
+        ],
+      )
+    );
+  }
+
+  Widget _crearAppbar(Pelicula pelicula){
+    return SliverAppBar(
+      elevation: 2.0,
+      backgroundColor: Colors.indigoAccent,
+      expandedHeight: 200.0,
+      floating: false,
     );
   }
 }
